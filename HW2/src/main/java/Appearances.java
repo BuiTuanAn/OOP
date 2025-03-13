a255c124f33fbb3be4d33fe6fa5e51a2f8a85524
BuiTuanAn <24022245@vnu.edu.vn>
2025-03-14 01:55:07 +0700

    catnhat bai HW2

--- HW2/src/main/java/Appearances.java
+++ HW2/src/main/java/Appearances.java
@@ -8,7 +8,21 @@ public class Appearances {
 	 * @return number of same-appearance elements
 	 */
 	public static <T> int sameCount(Collection<T> a, Collection<T> b) {
-		return 0; // YOUR CODE HERE
+
+
+		Map<T, Integer> freqA = getFrequencyMap(a);
+		Map<T, Integer> freqB = getFrequencyMap(b);
+
+		int count = 0;
+
+		for (T key : freqA.keySet()) {
+			if (freqB.containsKey(key) && freqA.get(key).equals(freqB.get(key))) {
+				count++;
+			}
+		}
+
+		return count;
+	}
 	}
 	
 }
--- HW2/src/main/java/StringCode.java
+++ HW2/src/main/java/StringCode.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,25 +9,55 @@ public class StringCode {
 	/**
 	 * Given a string, returns the length of the largest run.
 	 * A a run is a series of adajcent chars that are the same.
+	 *
 	 * @param str
 	 * @return max run length
 	 */
+	int d = 0;
+
 	public static int maxRun(String str) {
-		return 0; // YOUR CODE HERE
+		int d = 1;
+		int max = 0;
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (str.charAt(i) == str.charAt(i + 1)) {
+				d = d + 1;
+			} else {
+				if (d > max)
+					max = d;
+				d = 0;
+			}
+
+
+		}
+		return max;
+
 	}
 
-	
+
 	/**
 	 * Given a string, for each digit in the original string,
 	 * replaces the digit with that many occurrences of the character
 	 * following. So the string "a3tx2z" yields "attttxzzz".
+	 *
 	 * @param str
 	 * @return blown up string
 	 */
 	public static String blowup(String str) {
-		return null; // YOUR CODE HERE
+		StringBuilder str2 = new StringBuilder(str);
+		for (int i = 0; i < str2.length() - 1; i++) {
+			if (Character.isDigit(str2.charAt(i))) {
+				int n = str2.charAt(i) - '0';
+				String test = "";
+				for (int j = 1; j <= n; j++)
+					test = test + str2.charAt(i + 1);
+				str2.replace(i, i + 1, test);
+				i = i + n - 1;
+			}
+
+		}
+		return str2.toString();
 	}
-	
+
 	/**
 	 * Given 2 strings, consider all the substrings within them
 	 * of length len. Returns true if there are any such substrings
